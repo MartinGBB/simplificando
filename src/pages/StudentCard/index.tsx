ceac6e5ffaeac1bac247187cb11cb2e20cc45089
Martin <escorpmartin97@gmail.com>
2022-11-20 19:11:49 -0300

    feat: valida carregamento de dados no card de estudante

--- src/pages/StudentCard/index.tsx
+++ src/pages/StudentCard/index.tsx
@@ -1,9 +1,13 @@
 import { useParams } from "react-router-dom"
 
 import { CartContainer } from "./styles";
+import { Loading } from "../../components/Loading";
 
 export function StudentCard() {
   const { studentId } = useParams()
+
+  const isLoading = false
+  if (isLoading) return <Loading />
 
   return (
     <CartContainer>
