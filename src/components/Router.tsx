8a9fe01b11d99c5923734c045bf946bb42cd0462
Martin <escorpmartin97@gmail.com>
2022-11-20 18:18:01 -0300

    fix: corrige importacao do layout

--- src/components/Router.tsx
+++ src/components/Router.tsx
@@ -1,10 +1,10 @@
 import { Routes, Route } from 'react-router-dom'
-import { LayoutContainer } from '../layouts/DefaultLayout/styles'
+import { DefaultLayout } from '../layouts/DefaultLayout'
 
 export function Router() {
   return (
     <Routes>
-      <Route path="/" element={<LayoutContainer />}>
+      <Route path="/" element={<DefaultLayout />}>
         <Route path="/" element={<h1>Home</h1>} />
       </Route>
     </Routes>
