30c170bd5d11c67501520cd386694a225305a5ea
Martin <escorpmartin97@gmail.com>
2022-11-20 18:30:19 -0300

    feat: adiciona componente de carga

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,6 +1,11 @@
 import { HomeContainer } from "./styles";
+import { Loading } from "../../components/Loading";
 
 export function Home() {
+  const isLoading = false
+
+  if (isLoading) return <Loading />
+
   return (
     <HomeContainer>
       <h1>Home</h1>
